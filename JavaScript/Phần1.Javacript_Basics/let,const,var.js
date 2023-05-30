ed15d1b4c763598728276c86738c0845c8443bde
Đinh Văn Khôi <dinhvankhoi.vn@outlook.com>
2023-05-30 15:32:08 +0700

    sự khác biệt var , let , const

---
+++ JavaScript/Phần1.Javacript_Basics/let,const,var.js
@@ -0,0 +1,19 @@
+var number = 57;
+console.log(number);
+var number = `hello world`;
+console.log(number);
+
+let name = `Khôi`;
+console.log(name);
+
+name = 45;
+console.log(name);
+
+const year = 2024;
+console.log(year);
+
+/**
+ * ! var là phạm vi global , có thể tạo biến , gán lại giá trị , khai báo lại tên biến với giá trị khác  .
+ * ! let là phạm vi local , có thể tạo biến , gán lại giá trị , không khai báo trùng tên biến  .
+ * ! const là hằng số , không thể gán giá trị , thực thi trong suốt quá trình không thay đổi giá trị
+ */
