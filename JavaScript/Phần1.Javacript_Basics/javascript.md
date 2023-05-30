794fccbe1c273803c0f0c67cad459d1f110d1432
Đinh Văn Khôi <dinhvankhoi.vn@outlook.com>
2023-05-30 21:03:46 +0700

    delete

--- JavaScript/Phần1.Javacript_Basics/javascript.md
+++
@@ -1,1 +0,0 @@
-# Javascript được thực thi , khi tạo trong file riêng ,
--- JavaScript/Phần1.Javacript_Basics/numbers.js
+++
@@ -1,7 +0,0 @@
-let num1 = 56;
-let result = num1 + 5;
-console.log(result);
-
-let num2 = 5;
-let result2 = (num2 **= num2);
-console.log(result2);
