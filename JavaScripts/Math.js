a1e85de357869552a368840fce978c16ce562b78
Đinh Văn Khôi <dinhvankhoi.vn@outlook.com>
2023-06-01 21:59:35 +0700

    các phương thức làm việc với Math

---
+++ JavaScripts/Math.js
@@ -0,0 +1,15 @@
+let x = 5.9;
+let result = Math.floor(x);
+result = Math.ceil(x);
+result = Math.round(x);
+result = Math.trunc(x);
+result = Math.floor(Math.random() * 10);
+
+const number = [4,6,7,9,2,3,5,6];
+Math.min(...number);
+Math.max(...number);
+console.log(Math.min(...number) , Math.max(...number));
+console.log(result);
+Math.pow(2,3);
+Math.sqrt(9);
+Math.PI;
