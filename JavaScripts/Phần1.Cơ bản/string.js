160a0e85fd586cdaf1e37b5b8d1b2bb8f1e245b4
Đinh Văn Khôi <dinhvankhoi.vn@outlook.com>
2023-06-01 13:37:42 +0700

    khác biệt giữa biến

---
+++ JavaScripts/Phần1.Cơ bản/string.js
@@ -0,0 +1,9 @@
+let name = `Đinh Văn Khôi `;
+let age = 25 ; 
+let address = `Quảng Nam`;
+
+console.log(`tôi tên là ${name} hiện tại ${age}  tuổi , đang sinh sông tại ${address}`);
+
+// ! var là phạm vi global , có thể thay đổi , gán biến , đặt cùng biến . 
+// ! let là phạm vi local , có thể thay đổi , gán biến ,  không đặt cùng biến .
+// ! const là hằng số , không thể tạo biến  mà không gán giá trị  . 
