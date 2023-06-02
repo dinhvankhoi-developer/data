7f43906413bf737b2d7ea9a192eed037508216ef
Đinh Văn Khôi <dinhvankhoi.vn@outlook.com>
2023-06-02 23:13:51 +0700

    cách js vận hành :  , khi tạo biến , dữ liệu thì nó lưu vào bộ nhớ dưới dạng key - value , tuỳ thuộc vào kiểu dữ liệu nguyên thuỷ  & tham chiếu   sẽ lưu vào stack hay heap , điều này là nó đã tạo ra bộ nhớ lưu trữ  để lưu dữ liệu , khi thực thi , hay là gọi gọi hàm , thì nó sẽ tạo ra ngữ cảnh thực thi mới để thực hiện chức năng và trả về kết quả

---
+++ JavaScripts/Phần3.Function_Scope/excution_context.js
@@ -0,0 +1,15 @@
+let a = 48;
+let b = 45;
+
+const sum = (a, b) => {
+  return a + b;
+};
+
+console.log(sum(a, b));
+
+const populationPeople = (number) => {
+  // tham số
+  let result = number / 7900;
+  console.log(`dân số trên thế giới là ${result}`);
+};
+populationPeople(7900); // đối số
