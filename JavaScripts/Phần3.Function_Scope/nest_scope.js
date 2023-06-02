9444ee7835db3b8303facb7a54003dd8511ecbf9
Đinh Văn Khôi <dinhvankhoi.vn@outlook.com>
2023-06-02 17:57:33 +0700

    ví  dụ cơ bản về Clourse JS

--- JavaScripts/Phần3.Function_Scope/nest_scope.js
+++ JavaScripts/Phần3.Function_Scope/nest_scope.js
@@ -14,3 +14,36 @@ const showPerson = () => {
 
 let rest = showPerson();
 console.log(rest());
+
+const hinhVuong = () => {
+  let x = 10;
+  let y = 25;
+  const showRectangle = () => {
+    return console.log(`x = ${x}, y = ${y}`);
+  };
+
+  const showCircle = () => {
+    let sum = (x + y) * 2;
+    return sum; // lưu giá trị vào biến
+  };
+
+  const showArea = () => {
+    let area = x * y;
+    return area; // lưu giá trị vào biến
+  };
+
+  const showPerimeter = () => {
+    let result = {
+      sum: showCircle(),
+      area: showArea(),
+      rectangle: showRectangle(), // lưu kết quả từ console.log vào thuộc tính rectangle
+    };
+    return result; // trả về object
+  };
+
+  return showPerimeter(); // lưu object trả về vào biến khác rồi truy cập các thuộc tính tương ứng.
+};
+const result = hinhVuong();
+console.log(result.sum); // output: 70
+console.log(result.area); // output: 250
+console.log(result.rectangle); // output: 'x = 10, y = 25'
