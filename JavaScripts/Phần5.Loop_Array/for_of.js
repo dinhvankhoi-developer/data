f082652a3e3245ec38d5afc283b1b830e60d2800
Đinh Văn Khôi <dinhvankhoi.vn@outlook.com>
2023-06-04 00:08:47 +0700

    for of là duyệt qua mảng,  hay chuỗi string ,  để lấy dữ liệu , vì là vòng lặp for of nên nó những thuộc tính , và phương thức làm việc với array nếu như xác  định dữ liệu lặp là array , còn dữ liệu là chuỗi  , thì dùng phương thức string để làm việc

---
+++ JavaScripts/Phần5.Loop_Array/for_of.js
@@ -0,0 +1,8 @@
+let str = `Đinh Văn Khôi`;
+if (str.includes("Khôi")) {
+  console.log(`Chuỗi "${str}" chứa từ "Khôi".`);
+  let result = str.includes("Khôi");
+  console.log(result);
+} else {
+  console.log(`Không tìm thấy từ "Khôi" trong chuỗi "${str}".`);
+}
