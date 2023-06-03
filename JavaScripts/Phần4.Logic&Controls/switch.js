6afe3fc7e0cd64684e4d0749df1ec6c8aec616a3
Đinh Văn Khôi <dinhvankhoi.vn@outlook.com>
2023-06-03 15:48:11 +0700

    câu lệnh điều kiện switch  , dùng khi mình muốn kiểm tra nhiều trường hợp khác  nhau  , dùng break để thoát điều kiện

---
+++ JavaScripts/Phần4.Logic&Controls/switch.js
@@ -0,0 +1,69 @@
+const date = new Date();
+const month = date.getMonth() + 1;
+
+switch (month) {
+  case 1:
+    setTimeout(() => {
+      console.log(`Đây là Tháng 1`);
+    }, 1000);
+    break;
+  case 2:
+    setTimeout(() => {
+      console.log(`Đây là Tháng 2`);
+    }, 1000);
+    break;
+  case 3:
+    setTimeout(() => {
+      console.log(`Đây là Tháng 3`);
+    }, 1000);
+    break;
+  case 4:
+    setTimeout(() => {
+      console.log(`Đây là Tháng 4`);
+    }, 1000);
+    break;
+  case 5:
+    setTimeout(() => {
+      console.log(`Đây là Tháng 5`);
+    }, 1000);
+    break;
+  case 6:
+    setTimeout(() => {
+      console.log(`Đây là Tháng 6`);
+    }, 1000);
+    break;
+  case 7:
+    setTimeout(() => {
+      console.log(`Đây là Tháng 7`);
+    }, 1000);
+    break;
+  case 8:
+    setTimeout(() => {
+      console.log(`Đây là Tháng 8`);
+    }, 1000);
+    break;
+  case 9:
+    setTimeout(() => {
+      console.log(`Đây là Tháng 9`);
+    }, 1000);
+    break;
+  case 10:
+    setTimeout(() => {
+      console.log(`Đây là Tháng 10`);
+    }, 1000);
+    break;
+  case 11:
+    setTimeout(() => {
+      console.log(`Đây là Tháng 11`);
+    }, 1000);
+    break;
+  case 12:
+    setTimeout(() => {
+      console.log(`Đây là Tháng 12`);
+    }, 1000);
+    break;
+
+  default:
+    console.log("Không phải tháng trong năm.");
+    break;
+}
