d2d54b9f8728656caf3f3e0a8e23a6573fd4781e
Đinh Văn Khôi <dinhvankhoi.vn@outlook.com>
2023-06-03 15:56:25 +0700

    phân biệt giá trị truthy & falsy

---
+++ JavaScripts/Phần4.Logic&Controls/truthy_falsy.js
@@ -0,0 +1,27 @@
+let a = 0;
+let b = "";
+let c = "";
+let d = false;
+let e = null;
+let f = undefined;
+let g = NaN;
+
+if (Boolean(a)) {
+  console.log(`true`);
+} else if (Boolean(b)) {
+  console.log(`true`);
+} else if (Boolean(c)) {
+  console.log(`true`);
+} else if (Boolean(d)) {
+  console.log(`true`);
+} else if (Boolean(e)) {
+  console.log(`true`);
+} else if (Boolean(f)) {
+  console.log(`true`);
+} else if (Boolean(g)) {
+  console.log(`true`);
+} else {
+  console.log(`false`);
+}
+
+// ! những giá trị falsy là những giá trị trên , còn giá trị khác falsy là giá trị đúng .
